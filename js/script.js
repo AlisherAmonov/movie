2d1e465c2cf334631df075b6d9cc3d25dcc6e8c8
Alisher Amonov <amsherov@gmail.com>
2025-02-10 20:23:07 -0500

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,50 +1,98 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict'
 
-const movieDB = {
-  movies: [
-    'LOGAN',
-    'JUSTICE LEAGUE',
-    'LA LA LAND',
-    'OBSESSION',
-    'SCOTT PILGRIM VS...',
-  ],
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const movieDB = {
+    movies: [
+      'LOGAN',
+      'JUSTICE LEAGUE',
+      'LA LA LAND',
+      'OBSESSION',
+      'SCOTT PILGRIM VS...',
+    ],
+  }
 
-const adv = document.querySelectorAll('.promo__adv img')
-const poster = document.querySelector('.promo__bg')
-const genre = poster.querySelector('.promo__genre')
-const movieList = document.querySelector('.promo__interactive-list')
+  const adv = document.querySelectorAll('.promo__adv img')
+  const poster = document.querySelector('.promo__bg')
+  const genre = poster.querySelector('.promo__genre')
+  const movieList = document.querySelector('.promo__interactive-list')
+  const addForm = document.querySelector('form.add')
+  const addInput = addForm.querySelector('.adding__input')
+  const checkbox = addForm.querySelector('[type="checkbox"]')
 
-adv.forEach((item) => {
-  item.remove()
-})
+  addForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-genre.textContent = 'drama'
+    let newFilm = addInput.value
+    const favorite = checkbox.checked
 
-poster.style.backgroundImage = 'url("img/bg.jpg")'
+    if (newFilm) {
+      if (newFilm.length > 21) {
+        newFilm = `${newFilm.substring(0, 22)}...`
+      }
+      if (favorite) {
+        console.log('Add your favorite movie')
+      }
 
-movieList.innerHTML = ''
+      movieDB.movies.push(newFilm)
+      sortMovies(movieDB.movies)
+      createMovieList(movieDB.movies, movieList)
+    }
+    e.target.reset()
+  })
 
-movieDB.movies.sort()
+  const deleteAdv = (arg) => {
+    arg.forEach((item) => {
+      item.remove()
+    })
+  }
 
-movieDB.movies.forEach((film, i) => {
-  movieList.innerHTML += `
-   <li class="promo__interactive-item">${i + 1}. ${film}
-            <div class="delete"></div>
-   </li>
-  `
+  const makeChanges = () => {
+    genre.textContent = 'drama'
+    poster.style.backgroundImage = 'url("img/bg.jpg")'
+  }
+
+  const sortMovies = (movie) => {
+    movie.sort()
+  }
+
+  function createMovieList(films, parent) {
+    parent.innerHTML = ''
+    sortMovies(films)
+
+    films.forEach((film, i) => {
+      parent.innerHTML += `
+             <li class="promo__interactive-item">${i + 1}. ${film}
+                      <div class="delete"></div>
+             </li>
+            `
+    })
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+      btn.addEventListener('click', () => {
+        btn.parentElement.remove()
+        movieDB.movies.splice(i, 1)
+        createMovieList(films, parent)
+      })
+    })
+  }
+
+  createMovieList(movieDB.movies, movieList)
+  makeChanges()
+  deleteAdv(adv)
 })
